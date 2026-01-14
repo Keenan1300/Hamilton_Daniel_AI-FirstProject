4b2cbbdd97bbd489519ea4f3af7224c9cdd3053c
Keenan1300 <hamilt25@sheridancollege.ca>
2026-01-14 04:26:45 -0500

    addedactions

--- Assets/Scripts/StandbyAT.cs
+++ Assets/Scripts/StandbyAT.cs
@@ -5,30 +5,33 @@ using ParadoxNotion.Design;
 
 namespace NodeCanvas.Tasks.Actions {
 
-	public class DisableAT : ActionTask {
+	public class StandbyAT : ActionTask {
 
-		
-		//Use for initialization. This is called only once in the lifetime of the task.
-		//Return null if init was successfull. Return an error string otherwise
-		protected override string OnInit() {
+        public int num = 2;
+		public Material Standby; 
+
+        //Use for initialization. This is called only once in the lifetime of the task.
+        //Return null if init was successfull. Return an error string otherwise
+        protected override string OnInit() {
 			return null;
-		}
 
 		//This is called once each time the task is enabled.
 		//Call EndAction() to mark the action as finished, either in success or failure.
 		//EndAction can be called from anywhere.
 		protected override void OnExecute() {
 
-			//agent is vehicle to get access to game object content, translator for non-monobehaviour type of script like this
-			agent.gameObject.SetActive(false);
-			agent.transform.position += Vector3.right;
-			agent.GetComponent<Rigidbody>();
-			//end action stops behaviours
-			EndAction(true);
+            Renderer renderer = agent.gameObject.GetComponent<Renderer>();
+            //renderer.material = ;
+            agent.transform.position += Vector3.right;
+            agent.GetComponent<Rigidbody>();
+
+            //end action stops behaviours
+            EndAction(true);
 		}
 
 		//Called once per frame while the action is active.
 		protected override void OnUpdate() {
+			
 		}
 
 		//Called when the task is disabled.
--- Assets/Scripts/TargetAT.cs
+++ Assets/Scripts/TargetAT.cs
@@ -1,13 +1,11 @@
 using NodeCanvas.Framework;
-using UnityEngine;
 using ParadoxNotion.Design;
 
 
 namespace NodeCanvas.Tasks.Actions {
 
-	public class DisableAT : ActionTask {
+	public class TargetAT : ActionTask {
 
-		
 		//Use for initialization. This is called only once in the lifetime of the task.
 		//Return null if init was successfull. Return an error string otherwise
 		protected override string OnInit() {
@@ -18,17 +16,12 @@ namespace NodeCanvas.Tasks.Actions {
 		//Call EndAction() to mark the action as finished, either in success or failure.
 		//EndAction can be called from anywhere.
 		protected override void OnExecute() {
-
-			//agent is vehicle to get access to game object content, translator for non-monobehaviour type of script like this
-			agent.gameObject.SetActive(false);
-			agent.transform.position += Vector3.right;
-			agent.GetComponent<Rigidbody>();
-			//end action stops behaviours
 			EndAction(true);
 		}
 
 		//Called once per frame while the action is active.
 		protected override void OnUpdate() {
+			
 		}
 
 		//Called when the task is disabled.
