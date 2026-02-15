fab566793d1d0c0980d9be9337f2141b62d76f6b
Keenan1300 <hamilt25@sheridancollege.ca>
2026-02-15 05:11:38 -0500

    Added Dragon Fatigue location

--- Assets/Scripts/Project1/SleepAT.cs
+++ Assets/Scripts/Project1/SleepAT.cs
@@ -1,26 +1,51 @@
 using NodeCanvas.Framework;
 using ParadoxNotion.Design;
-
+using UnityEngine;
+using UnityEngine.AI;
+using System.Collections.Generic;
+using UnityEngine.UIElements;
+using Unity.VisualScripting;
 
 namespace NodeCanvas.Tasks.Actions {
 
 	public class SleepAT : ActionTask {
 
-		//Use for initialization. This is called only once in the lifetime of the task.
-		//Return null if init was successfull. Return an error string otherwise
-		protected override string OnInit() {
-			return null;
+        public BBParameter<Transform> sleepspot;
+        public BBParameter<float> IdleTime;
+        private NavMeshAgent navAgent;
+
+
+
+
+
+        //Use for initialization. This is called only once in the lifetime of the task.
+        //Return null if init was successfull. Return an error string otherwise
+        protected override string OnInit() {
+            navAgent = agent.GetComponent<NavMeshAgent>();
+            return null;
 		}
 
 		//This is called once each time the task is enabled.
 		//Call EndAction() to mark the action as finished, either in success or failure.
 		//EndAction can be called from anywhere.
 		protected override void OnExecute() {
-			EndAction(true);
-		}
+            SetDestination();
+        }
 
-		//Called once per frame while the action is active.
-		protected override void OnUpdate() {
+
+        private void SetDestination()
+        {
+   
+            Vector3 Sleeppoint = sleepspot.value.position;
+            
+            navAgent.SetDestination(Sleeppoint);
+            
+
+        }
+
+
+        //Called once per frame while the action is active.
+        protected override void OnUpdate() {
 			
 		}
 
